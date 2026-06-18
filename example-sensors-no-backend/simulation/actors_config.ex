a5e5615d7c0f5020cde5652bd9faa9d7ab6b08bf
Luca Paolella <luca.paolella@gmail.com>
2026-06-19 00:48:41 +0200

    minor fixes

--- example-sensors-no-backend/simulation/actors_config.ex
+++ example-sensors-no-backend/simulation/actors_config.ex
@@ -1,8 +1,8 @@
 defmodule Beamulator.ActorsConfig do
   def actors() do
     [
-      %{role: Beamulator.Roles.Collector, name: "Collector", amt: 100, config: %{}},
-      %{role: Beamulator.Roles.Sensor, name: "Sensor", amt: 3000, config: %{}}
+      %{role: Beamulator.Roles.Collector, name: "Collector", amt: 10, config: %{}},
+      %{role: Beamulator.Roles.Sensor, name: "Sensor", amt: 300, config: %{}}
     ]
   end
 end
